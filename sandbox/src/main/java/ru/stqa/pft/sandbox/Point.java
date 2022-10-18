0de26f0fe2f7c7f40cd2281e237258177f731cf4
miroshnik24 <115870908+miroshnik24@users.noreply.github.com>
2022-10-18 23:22:02 +0700

    Домашнее задание №2 (исправление)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,12 +1,24 @@
 package ru.stqa.pft.sandbox;
 
+//import static java.lang.System.*;
+
 public class Point {
-  public static void main (String[] args) {
-    Pointdate P = new Pointdate(5,3,2,-7);
-    System.out.println("Расстояние равно " + distance (P));
+  public static double p1;
+  private static double p2;
+
+  public static void main(String[] args) {
+    Point p1 = new Point();
+    Point p2 = new Point();
+
+    Point.p1 = 4;
+    Point.p2 = 10;
+
+    System.out.println("Расстояние между точками (" + Point.p1 + ") и (" + Point.p2 + ") = " + distance());
   }
 
-  public static double distance (Pointdate P) {
-    return Math.sqrt((P.p3 - P.p1)*(P.p3 - P.p1) + (P.p4 - P.p2) * (P.p4 - P.p2));
+  public static double distance() {
+    return Math.sqrt(p2 - p1);
   }
+
+
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Pointdate.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Pointdate.java
@@ -1,15 +1,14 @@
 package ru.stqa.pft.sandbox;
 
 public class Pointdate {
-    public static double p1;
-    public static double p2;
-    public static double p3;
-    public static double p4;
+  public static double p1;
+  public static double p2;
 
-    public Pointdate(double p1, double p2, double p3, double p4) {
-      this.p1 = p1;
-      this.p2 = p2;
-      this.p3 = p3;
-      this.p4 = p4;
-    }
+
+  public Pointdate() {
+    p1 = p1;
+    p2 = p2;
   }
+}
+
+
