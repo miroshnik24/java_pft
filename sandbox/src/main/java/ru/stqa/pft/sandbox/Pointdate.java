ded4cc5f33e710ce7c60ed0a3ca5738f35d88e36
miroshnik24 <115870908+miroshnik24@users.noreply.github.com>
2022-10-23 23:13:51 +0700

    Задание №2 (версия 3)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Pointdate.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Pointdate.java
@@ -1,14 +1,26 @@
 package ru.stqa.pft.sandbox;
 
 public class Pointdate {
-  public static double p1;
-  public static double p2;
 
-
-  public Pointdate() {
-    p1 = p1;
-    p2 = p2;
+  public static void main(String[] args) {
+    Point p1 = new Point(1,2);
+    Point p2 = new Point(4,3);
+    System.out.println("Расстояние между точками = "+p1.distance(p2));
   }
+
 }
 
+class Point{
+  private final double x;
+  private final double y;
 
+  public Point(double x1, double y1) {
+    this.x = x1;
+    this.y = y1;
+  }
+
+  public double distance(Point p2){
+    return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
+  }
+
+}
