00e89744724f87faace643a63e19dd643678c2ca
miroshnik24 <115870908+miroshnik24@users.noreply.github.com>
2022-10-30 18:20:40 +0700

    Задание №3 (версия 2)

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -14,11 +14,17 @@ public class PointTests {
 
   @Test
   public void TestArea2() {
-    Point p1 = new Point(10, 5);
-    Point p2 = new Point(7, 15);
-    Assert.assertEquals(p1.distance(p2), -25);
+    Point p1 = new Point(-5, 5);
+    Point p2 = new Point(17, 0);
+    Assert.assertEquals(p1.distance(p2), 22.561028345356956);
   }
 
+  @Test
+  public void TestArea3() {
+    Point p1 = new Point(99, -101);
+    Point p2 = new Point(100, -99);
+    Assert.assertEquals(p1.distance(p2), 2.23606797749979);
+  }
 
 }
 
