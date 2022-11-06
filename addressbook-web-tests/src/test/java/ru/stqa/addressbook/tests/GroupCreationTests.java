a88d0a52be5dec9e238971870211d962d96c4f86
miroshnik24 <115870908+miroshnik24@users.noreply.github.com>
2022-11-06 20:41:40 +0700

    Лекция 3.6. Оптимизация заполнения форм

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
@@ -11,7 +11,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getNovigationHelper().gotoGroupPage();
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
     app.getGroupHelper().wd.findElement(By.linkText("Logout")).click();
