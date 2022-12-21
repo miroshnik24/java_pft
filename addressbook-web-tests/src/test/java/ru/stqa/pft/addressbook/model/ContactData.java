e16e122254f2af910f1ae1b1261f7cf7007ee82f
miroshnik24 <115870908+miroshnik24@users.noreply.github.com>
2022-12-21 23:39:35 +0700

    21/12 4

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -4,7 +4,6 @@ import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 import org.hibernate.annotations.Type;
-import ru.stqa.pft.addressbook.tests.ContactCheck;
 
 import javax.persistence.*;
 import java.io.File;
