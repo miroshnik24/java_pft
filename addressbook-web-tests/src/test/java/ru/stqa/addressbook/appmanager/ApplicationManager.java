130b473c082d37b75dda4324620c22447adcc429
miroshnik24 <115870908+miroshnik24@users.noreply.github.com>
2022-12-22 00:51:07 +0700

    Реализована возможность удаленного запуска браузера

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/ApplicationManager.java
@@ -1,59 +1,91 @@
 package ru.stqa.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-public class ApplicationManager {
+public class ApplicationManager
+{
+  private final Properties properties;
   WebDriver wd;
-
   private ContactHelper contactHelper;
-  private NovigationHelper novigationHelper;
+  private NovigationHelper navigationHelper;
   private GroupHelper groupHelper;
-  private final String browser;
+  private SessionHelper sessionHelper;
+  private DbHelper dbHelper;
+  private String browser;
 
-  public ApplicationManager(String browser) {
+  public ApplicationManager(String browser)
+  {
     this.browser = browser;
+    properties = new Properties();
   }
 
-  public void init() {
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver(new FirefoxOptions().setBinary("C:/Program Files/Mozilla Firefox/firefox.exe"));
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver(new ChromeOptions().setBinary("C:/Program Files/Google/Chrome/Application/chrome.exe"));
-    } else if (browser.equals(BrowserType.EDGE)) {
-      wd = new EdgeDriver();
+  public void init() throws IOException
+  {
+    String target = System.getProperty("target", "local");
+    dbHelper = new DbHelper();
+
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+    if ("".equals(properties.getProperty("selenium.server"))) {
+      if (browser.equals( BrowserType.FIREFOX )) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals( BrowserType.CHROME )) {
+        wd = new ChromeDriver();
+      } else if (browser.equals( BrowserType.IE )) {
+        wd = new InternetExplorerDriver();
+      }
+    } else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
+
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook");
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
-    novigationHelper = new NovigationHelper(wd);
-    SessionHelper sessionHelper = new SessionHelper(wd);
+    navigationHelper = new NovigationHelper(wd);
+    sessionHelper = new SessionHelper(wd);
     contactHelper = new ContactHelper(wd);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
   }
 
-  public void stop() {
+  public void stop()
+  {
+    sessionHelper.logout();
     wd.quit();
   }
 
-
-  public GroupHelper group() {
+  public GroupHelper group()
+  {
     return groupHelper;
   }
 
-  public NovigationHelper goTo() {
-    return novigationHelper;
+  public NovigationHelper goTo()
+  {
+    return navigationHelper;
   }
 
-  public ContactHelper contact() {
+  public ContactHelper contact()
+  {
     return contactHelper;
   }
 
+  public DbHelper db()
+  {
+    return dbHelper;
+  }
 }
