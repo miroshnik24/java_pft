6431c3778ed60b8f01ee481aff7d355c1d9de6da
miroshnik24 <115870908+miroshnik24@users.noreply.github.com>
2023-01-10 23:00:49 +0700

    Задание №16. Попытка 6

---
+++ addressbook/tests/ContactAddInGroupTest.java
@@ -0,0 +1,45 @@
+package ru.stqa.addressbook.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.addressbook.model.ContactData;
+import ru.stqa.addressbook.model.Contacts;
+import ru.stqa.addressbook.model.GroupData;
+import ru.stqa.addressbook.model.Groups;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ContactAddInGroupTest extends TestBase {
+
+  @BeforeMethod
+  public void ensurePreconditions() {
+    if (app.db().contacts().size() == 0) {
+      Groups groups = app.db().groups();
+      app.goTo().HomePage();
+      app.contact().create(new ContactData("Иван", "Васильевич", "Иванов", null, "221-65-52", "89185555550","456-55-51",
+              "1.ru", "2.ru", "3.ru",null),true, groups.iterator().next().getName());
+      app.goTo().HomePage();
+    }
+
+    if (app.db().groups().size() == 0) {
+      app.goTo().GroupPage();
+      app.group().create(new GroupData("test1", "test2", "test3"));
+      app.goTo().returnToGroupPage();
+    }
+  }
+
+  @Test
+  public void testContactAddInGroup() {
+    app.goTo().HomePage();
+    ContactData addedContact = app.db().contacts().iterator().next();
+    GroupData group = app.db().groups().iterator().next();
+    Contacts before = group.getContacts();
+    app.contact().addInGroup(addedContact, group);
+    app.goTo().GroupPageByName(group.getName());
+    GroupData groupAfter = app.db().groupById(group.getId()).iterator().next();
+    Contacts after = groupAfter.getContacts();
+    assertThat(after, equalTo(before.without(addedContact).withAdded(addedContact)));
+    verifyContactListInGroupInUI(groupAfter);
+  }
+}
